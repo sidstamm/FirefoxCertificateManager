fa0ed869cf0b5f83a6e5530490e39292e092a3b0
Dax <dax.earl@gmail.com>
2015-04-30 14:37:37 -0400

    added gheto javascript

--- addon/content/tabOverlay.js
+++ addon/content/tabOverlay.js
@@ -18,6 +18,52 @@ CertManager.TabOverlay = {
    	window.alert(message);
   }
 };
+var a;
+
+function click_row(st,ev){
+  row = ev.target;
+  console.log(ev)
+  if(ev.layerX>595 && ev.layerX<640){
+    toggle_checkbox(row);
+  }
+  if(ev.layerX>2 && ev.layerX<15){
+    toggle_arrow(row)
+  }
+  a = ev;
+}
+
+function toggle_arrow(){
+  box = row.getElementsByClassName("arrow")[0]
+  status = box.getAttribute("direction")
+  if(status=="right"){
+    box.innerHTML = "&#8595;"
+    status = box.setAttribute("direction","down")
+  }
+  else{
+    box.innerHTML = "&#8594;"
+    status = box.setAttribute("direction","right")
+  }
+}
+
+function checked(row){
+  box = row.getElementsByClassName("checkbox")[0]
+  check = box.getAttribute("checked")
+  if(check=="false"){
+    return false;
+  }
+  return true;
+}
+
+function toggle_checkbox(row){
+  box = row.getElementsByClassName("checkbox")[0]
+
+  if(checked(row)){
+    box.setAttribute("checked","false")
+  }
+  else{
+    box.setAttribute("checked","true")
+  }
+}
 
 var gCertPane = {
   init: function ()
