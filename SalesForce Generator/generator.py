3f0595c6aeb75698bfd6e8f106854cea7d36b57f
Melissa Thai <thaimp@rose-hulman.edu>
2015-05-20 23:54:38 -0400

    made script to return most recent audit date and union trust bits per cert

--- SalesForce Generator/generator.py
+++ SalesForce Generator/generator.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import ast
+from datetime import datetime
 
 pendingCertsPath = 'Pending CA Certificate Requests - Sheet1.csv'
 builtInCertsPath = 'BuiltInCAs - Sheet1.csv'
@@ -21,15 +23,15 @@ def getPendingCerts():
 	salesforceJson = {}
 
 	for row in reader:	
-		CA = {}
-		CA['trustBits'] = row[11]
-		CA['geographicFocus'] = row[17]
-		CA['auditDate'] = row[26]
+		cert = {}
+		cert['trustBits'] = row[11]
+		cert['geographicFocus'] = row[17]
+		cert['auditDate'] = row[26]
 
 		if not salesforceJson.has_key(row[1]):
-			salesforceJson[row[1]] = [CA]
+			salesforceJson[row[1]] = [cert]
 		else:
-			salesforceJson[row[1]].append(CA)
+			salesforceJson[row[1]].append(cert)
 
 	output.write(json.dumps(salesforceJson, indent=2))
 
@@ -43,16 +45,46 @@ def getBuiltInCerts():
 	salesforceJson = {}
 
 	for row in reader:	
-		CA = {}
-		CA['trustBits'] = row[9]
-		CA['auditDate'] = row[24]
+		cert = {}
+		cert['trustBits'] = row[9]
+		cert['auditDate'] = row[24]
 
 		if not salesforceJson.has_key(row[1]):
-			salesforceJson[row[1]] = [CA]	
+			salesforceJson[row[1]] = cert	
 		else:
-			salesforceJson[row[1]].append(CA)
+			salesforceJson[row[1]] = getMostRecentCert(salesforceJson[row[1]], cert)
 
 	output.write(json.dumps(salesforceJson, indent=2))
+
+def getMostRecentCert(cert1, cert2):
+	result = {}
+	date1 = None
+	date2 = None
+
+	if not cert1['auditDate'] == '':
+		date1 = datetime.strptime(cert1['auditDate'], '%Y.%m.%d')
+
+	if not cert2['auditDate'] == '':
+		date2 = datetime.strptime(cert2['auditDate'], '%Y.%m.%d')
+
+	bits1 = [x.strip() for x in cert1['trustBits'].split(';')]
+	bits2 = [x.strip() for x in cert2['trustBits'].split(';')]
+	bitsUnion = union(bits1, bits2)
+
+	if date1 == None and date2 == None:
+		result['auditDate'] = ''
+	elif date1 == None:
+		result['auditDate'] = date2.strftime('%Y.%m.%d')
+	elif date2 == None:
+		result['auditDate'] = date1.strftime('%Y.%m.%d')	
+	else:	
+		result['auditDate'] = max(date1, date2).strftime('%Y.%m.%d')
+	result['trustBits'] = '; '.join(bitsUnion)
+
+	return result
+
+def union(a, b):
+	return list(set(a) | set(b))
 
 def main():
 	getPendingCerts()
