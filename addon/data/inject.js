776bf8d26e681445467842eb19c1052a4670172d
Mason Schneier <mason@schneiderscode.com>
2015-10-05 04:25:47 -0400

    Added cert view and navigation as well as funciton to add the rows

--- addon/data/inject.js
+++ addon/data/inject.js
@@ -1,28 +1,32 @@
 
 self.port.on("insert_row",function insert_row(num, source, name, trust, last, country, trustbits){
-		var parent = '<tr class="parent" id="row$Num"><td>$Source</td> <td colspan="2">$Name</td> <td>$Trust</td></tr>';
+		var parent = '<tr class="parent" id="row$Num"><td>$Source</td> <td id="name$Num" colspan="2">$Name</td> <td>$Trust</td></tr>';
         parent = parent.replace('$Source', source);
         parent = parent.replace('$Name', name);
         parent = parent.replace('$Trust', trust);
-        parent = parent.replace('$Num', num);
+        parent = parent.replace(/\$Num/g, num)
 
         var sub1 = '<tr class="child-row$Num"><td>&nbsp;</td><td>Last Audit: </td><td>$Last</td></tr>';
-
         sub1 = sub1.replace('$Num', num)
         sub1 = sub1.replace('$Last', last)
 
         var sub2 = "<tr class='child-row$Num'><td>&nbsp;</td><td>Country: </td><td>$Country</td></tr>"
         sub2 = sub2.replace('$Num', num)
         sub2 = sub2.replace('$Country', country)
+
         var sub3 = "<tr class='child-row$Num'><td>&nbsp;</td><td>TrustBits: </td><td>$TrustBits</td></tr>"
         sub3 = sub3.replace('$Num', num)
         sub3 = sub3.replace('$TrustBits', trustbits)
 
-        var table = document.getElementById("cert_table");
+        var sub4 = "<tr class='child-row$Num'><td>&nbsp;</td><td>&nbsp;</td><td><button onclick='showDetails($Num);' class='moreButton'>View Certificates</button></td></tr>"
+        sub4 = sub4.replace(/\$Num/g, num)
+
+        var table = document.getElementById("auth_table");
         table.innerHTML += parent;
         table.innerHTML += sub1;
         table.innerHTML += sub2;
         table.innerHTML += sub3;
+        table.innerHTML += sub4;
         
         $('tr.parent')
         .css("cursor","pointer")
@@ -31,12 +35,20 @@ self.port.on("insert_row",function insert_row(num, source, name, trust, last, co
             $(this).siblings('.child-'+this.id).toggle();
         });
 
-          $('#row$Num'.replace('$Num', num)).click();
+        $('#row$Num'.replace('$Num', num)).click();
     }
 );
 
-// //Need to get cert data here somehow
-// var certs = ['Wells Fargo WellsSecure', 'Certificate Issuer Organization', 'Hellenic Academic and Research Institutions Cert. Authority'];
-    // for(var i = 0; i<certs.length; i++){
-        // insert_row(i, 'Firefox', certs[i], 100, certManagerJson[certs[i]]['auditDate'], 'USA', certManagerJson[certs[i]]['trustBits']);
-    // }
+// web, email, and software should be either "" or "checked"
+self.port.on("insert_cert", function insert_cert(num, name, builtin, web, email, software){
+    var parent = '<tr class="parent" id="row$Num"><td>$Name</td> <td>$Builtin</td> <td><input type="checkbox" $Web></input></td> <td><input type="checkbox" $Email></input></td> <td><input type="checkbox" $Software></input></td></tr>';
+    parent = parent.replace(/\$Num/g, num);
+    parent = parent.replace(/\$Name/g, name);
+    parent = parent.replace(/\$Builtin/g, builtin);
+    parent = parent.replace(/\$Web/g, web);
+    parent = parent.replace(/\$Email/g, email);
+    parent = parent.replace(/\$Software/g, software);
+
+    var table = document.getElementById("cert_table");
+    table.innerHTML += parent;
+});
