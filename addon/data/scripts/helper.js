7cdcee96c6f0e24b4432e161324dcd7d715eb210
Mason Schneier <mason@schneiderscode.com>
2015-10-20 03:34:37 -0400

    Added searchbar functionality

--- addon/data/scripts/helper.js
+++ addon/data/scripts/helper.js
@@ -12,12 +12,72 @@ function showDetails(num) {
 
     $("#main_table").toggle();
     $("#detail_table").toggle();
+    $("#certsSearch").toggle();
+    $("#authsSearch").toggle();
     $("#authName").html('<a href="#" onclick="showAuths()">&lt; Back</a> ' + $("#name" + num).text());
     $("#authName").show();
 }
 
 function showAuths() {
     $("#detail_table").toggle();
+    $("#certsSearch").toggle();
+    $("#authsSearch").toggle();
     $("#main_table").toggle();
     $("#authName").hide();
+}
+
+function searchMainTable(inputVal)
+{
+    $('#main_table > tbody > .parent').each(function(index, row)
+    {
+        var allCells = $(row).find('td:nth-child(2)');
+        if(allCells.length > 0)
+        {
+            var found = false;
+            allCells.each(function(index, td)
+            {
+                var regExp = new RegExp(inputVal, 'i');
+                if(regExp.test($(td).text()))
+                {
+                    found = true;
+                    return false;
+                }
+            });
+            if(found == true)
+                $(row).show();
+            else
+            {
+                $(row).hide();
+                $(row).siblings('.child-' + $(row).attr('id')).hide();
+            }
+        }
+    });
+}
+
+function searchDetailTable(inputVal)
+{
+    $('#detail_table > tbody > .parent').each(function(index, row)
+    {
+        var allCells = $(row).find('td:nth-child(1)');
+        if(allCells.length > 0)
+        {
+            var found = false;
+            allCells.each(function(index, td)
+            {
+                var regExp = new RegExp(inputVal, 'i');
+                if(regExp.test($(td).text()))
+                {
+                    found = true;
+                    return false;
+                }
+            });
+            if(found == true)
+                $(row).show();
+            else
+            {
+                $(row).hide();
+                $(row).siblings('.child-' + $(row).attr('id')).hide();
+            }
+        }
+    });
 }
