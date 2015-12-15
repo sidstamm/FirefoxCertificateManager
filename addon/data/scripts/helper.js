6f4974af07a70d896ae76e055278344fe5ff9a4b
Dax <dax.earl@gmail.com>
2015-12-14 22:42:56 -0500

    Removed duped code

--- addon/data/scripts/helper.js
+++ addon/data/scripts/helper.js
@@ -2,45 +2,6 @@
 	Any misc client side only javascript
 */
 
-function showDetails(num) {
-    var table = document.getElementById("cert_table");
-    while (table.hasChildNodes()) {
-        table.removeChild(table.firstChild);
-    }
-
-    window.listCerts(num);
-
-    $("#main_table").toggle();
-    $("#detail_table").toggle();
-    $("#certsSearch").toggle();
-    $("#authsSearch").toggle();
-    $("#authName").html('<a href="#" onclick="showAuths()">&lt; Back</a> ' + $("#name" + num).text());
-	$("#viewButton").show();
-	$("#exportButton").show();
-    $("#authName").show();
-    $("#delete").show();
-}
-
-function distrust(num) {
-    window.distrustAuth(num);
-
-    $("#distrust-"+num).attr('onclick', '').unbind().click(function() { entrust(num); })
-    $("#distrust-"+num).text("Trust");
-    $("#distrust-"+num).toggleClass("red");
-    $("#distrust-"+num).toggleClass('green');
-    $("#distrust-"+num).attr('id', 'entrust-'+num);
-}
-
-function entrust(num) {    
-    window.entrustAuth(num);
-
-    $("#entrust-"+num).attr('onclick', '').unbind().click(function() { distrust(num); })
-    $("#entrust-"+num).text("Distrust");    
-    $("#entrust-"+num).toggleClass("red");
-    $("#entrust-"+num).toggleClass('green');
-    $("#entrust-"+num).attr('id', 'distrust-'+num);
-}
-
 function showAuths() {
     $("#detail_table").toggle();
     $("#certsSearch").toggle();
@@ -53,12 +14,6 @@ function showAuths() {
     $("#delete").hide();
     $("#editTrustButton").hide();
     $("#footer").attr("id", "footer_plain");
-}
-
-function updateCertTrust(check) {
-    var classId = check.attr('class');
-    var allChecks = $("."+classId);
-    window.editCertTrust(classId.split('-')[0], classId.split('-')[1], allChecks[0].checked, allChecks[1].checked, allChecks[2].checked);
 }
 
 function searchMainTable(inputVal)
